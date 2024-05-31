5fbe36f6d7eb32d2ae009a678568676191dd7792
bfranks997 <67621416+bfranks997@users.noreply.github.com>
2024-05-31 02:02:43 -0400

    Fixed Dark Mode

--- Scripts/bradScript.js
+++ Scripts/bradScript.js
@@ -1,39 +1,33 @@
-// const darkModeButton = document.querySelector('.navbar .outer');
-// let theme = document.styleSheets[2];
-// console.log(theme);
-
 const darkModeButton = document.querySelector('.darkmode');
+let currentTheme = localStorage.getItem("currentTheme");
 
-let dark = false;
-// let moon = document.getElementById("svgClass");
-
-// moon.styles.stroke = "red";
-
-
-
-darkModeButton.addEventListener('click',()=>{
+darkModeButton.addEventListener('click', () => {
     console.log(document.getElementById("theme").href);
-    if(dark){
+    if (currentTheme === "true") {
         console.log('Enabling lightmode');
         document.getElementById("theme").href = "styles.css";
-        dark = false;
-    }else{
+        localStorage.setItem("currentTheme", "false");
+        currentTheme = "false"; // Update currentTheme variable
+        darkModeButton.textContent = "Light Mode";
+    } else {
         console.log('Enabling darkmode');
         document.getElementById("theme").href = "darkmode.css";
-        dark = true;
+        localStorage.setItem("currentTheme", "true");
+        currentTheme = "true"; // Update currentTheme variable
+        darkModeButton.textContent = "Dark Mode";
     }
 });
 
-
-window.onload = ()=>{
-    console.log("window loaded and darkmode is ",dark);
-    if(dark){
-        console.log('Enabling lightmode');
-        document.getElementById("theme").href = "styles.css";
-        dark = false;
-    }else{
-        console.log('Enabling darkmode');
+window.onload = () => {
+    console.log("window loaded and darkmode is ", currentTheme);
+    if (currentTheme === "true") {
+        console.log('Initializing darkmode');
         document.getElementById("theme").href = "darkmode.css";
-        dark = true;
+        darkModeButton.textContent = "Light Mode";
+    } else {
+        console.log('Initializing lightmode');
+        document.getElementById("theme").href = "styles.css";
+        localStorage.setItem("currentTheme", "false");
+        currentTheme = "false"; // Update currentTheme variable
     }
 };
